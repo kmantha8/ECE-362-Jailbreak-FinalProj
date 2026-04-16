19eeb5f28e3df7757e222aa45f5a30305711adad
ajstroth <ajstrothkamp@yahoo.com>
2026-04-16 17:56:55 -0400

    attempt spio

--- include/sdcard_hw.h
+++ include/sdcard_hw.h
@@ -5,11 +5,11 @@
 
 // Update these pins if your SD wiring differs.
 // The default mapping assumes SPI0 on GP16-19.
-#define SD_SPI_INSTANCE spi1
-#define SD_MISO_PIN 12
-#define SD_CS_PIN 13
-#define SD_SCK_PIN 14
-#define SD_MOSI_PIN 15
+#define SD_SPI_INSTANCE spi0
+#define SD_MISO_PIN 16
+#define SD_CS_PIN 17
+#define SD_SCK_PIN 18
+#define SD_MOSI_PIN 19
 
 void init_sdcard_io(void);
 void sdcard_io_high_speed(void);
